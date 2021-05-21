75a481a9535c748f279b75413976c6da234c2de5
mikael <mikael@FreeBSD.org>
2021-05-21 10:57:41 +0200

    multimedia/ringrtc: Update to 2.9.4

--- ports/multimedia/ringrtc/files/patch-cargo-crates_neon-0.7.1_src_lib.rs
+++ ports/multimedia/ringrtc/files/patch-cargo-crates_neon-0.7.1_src_lib.rs
@@ -1,5 +1,5 @@
---- cargo-crates/neon-0.6.0/src/lib.rs.orig	2021-03-15 17:51:55 UTC
-+++ cargo-crates/neon-0.6.0/src/lib.rs
+--- cargo-crates/neon-0.7.1/src/lib.rs.orig	2021-03-15 17:51:55 UTC
++++ cargo-crates/neon-0.7.1/src/lib.rs
 @@ -104,6 +104,7 @@ macro_rules! register_module {
      ($module:pat, $init:block) => {
          // Mark this function as a global constructor (like C++).
