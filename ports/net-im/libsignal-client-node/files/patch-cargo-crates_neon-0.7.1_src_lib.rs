940b9d76fae91f4fb53da2bef3c18a17386bbbce
mikael <mikael@FreeBSD.org>
2021-05-21 09:57:50 +0200

    net-im/libsignal-client-node: Update to 0.3.4
    This is the version supposedly compatible with Signal-Desktop 5.2.0-beta2

--- ports/net-im/libsignal-client-node/files/patch-cargo-crates_neon-0.7.1_src_lib.rs
+++ ports/net-im/libsignal-client-node/files/patch-cargo-crates_neon-0.7.1_src_lib.rs
@@ -1,5 +1,5 @@
---- cargo-crates/neon-0.5.3/src/lib.rs.orig	2021-03-15 14:07:45 UTC
-+++ cargo-crates/neon-0.5.3/src/lib.rs
+--- cargo-crates/neon-0.7.1/src/lib.rs.orig	2021-03-15 14:07:45 UTC
++++ cargo-crates/neon-0.7.1/src/lib.rs
 @@ -111,6 +111,7 @@ macro_rules! register_module {
      ($module:pat, $init:block) => {
          // Mark this function as a global constructor (like C++).
