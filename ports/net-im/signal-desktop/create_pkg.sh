7deb5f64404debaab5208c8a82319e123ae12420
MikaelUrankar <mikael.urankar@gmail.com>
2022-02-24 17:46:28 +0100

    Remove obsolete script

--- ports/net-im/signal-desktop/create_pkg.sh
+++
@@ -1,5 +0,0 @@
-env SIGNAL_ENV=production \
-yarn run electron-builder \
-                        --linux --dir --config.npmRebuild=false \
-                        --config.electronVersion=16 \
-                        --config.electronDist=/home/miki/.cache/electron/usr/local/share/electron16
