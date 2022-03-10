fa727fbb4129a3d82701d0f02c7abced57029ce3
MikaelUrankar <mikael.urankar@gmail.com>
2022-03-10 14:12:51 +0100

    net-im/signal-desktop: Update to 5.35.0

--- ports/net-im/signal-desktop/tar.sh
+++ ports/net-im/signal-desktop/tar.sh
@@ -1,12 +1,10 @@
 rm -rf work/.npm/_logs/ work/.npm/_update-notifier-last-checked
 rm -rf work/.cache/yarn/v6/.tmp
 
-find . -type f -perm 755 -exec file {} \; | egrep "ELF|PE32+|Mach-O" |awk -F ':' '{print $1}' | xargs rm
+find work/.cache  -type f -perm 755 -exec file {} \; | egrep "ELF|PE32+|Mach-O" |awk -F ':' '{print $1}' | xargs rm
 
 cd work
 
-
-
-tar czf ../signal-desktop-5.34.0-yarn-cache.tar.gz .cache yarn-cache
-tar czf ../signal-desktop-5.34.0-electron-gyp-cache.tar.gz .electron-gyp
-tar czf ../signal-desktop-5.34.0-npm-cache.tar.gz .npm
+tar czf ../signal-desktop-5.35.0-yarn-cache.tar.gz .cache yarn-cache
+tar czf ../signal-desktop-5.35.0-electron-gyp-cache.tar.gz .electron-gyp
+tar czf ../signal-desktop-5.35.0-npm-cache.tar.gz .npm
