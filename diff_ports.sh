295f9ac83cdcda2971220f2139c80b352eea08b6
MikaelUrankar <mikael.urankar@gmail.com>
2023-04-26 12:46:55 +0200

    Add diff_ports.sh

---
+++ diff_ports.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+diff -drup /usr/ports/net-im/signal-desktop	ports/net-im/signal-desktop
+diff -drup /usr/ports/net-im/libsignal-node	ports/net-im/libsignal-node
+diff -drup /usr/ports/multimedia/ringrtc	ports/multimedia/ringrtc
+
