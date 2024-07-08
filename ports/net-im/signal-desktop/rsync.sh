301eee40e953402baa5c8960764b8132cd86a0f4
Mikael Urankar <mikael@FreeBSD.org>
2024-07-08 14:34:25 +0200

    net-im/signal-desktop: Update to 7.15.0
    WIP

--- ports/net-im/signal-desktop/rsync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-signal_distfiles=$(make -V DISTFILES | awk /cache/'{print $1}' | sed 's/:yarn//')
-rsync -auv /usr/ports/distfiles/${signal_distfiles} root@azure:/mnt/resource/signal-desktop
--- ports/net-im/signal-desktop/setenv.sh
+++ ports/net-im/signal-desktop/setenv.sh
@@ -1,8 +1,11 @@
 setenv NODE_ENV production
 setenv SIGNAL_ENV production
-setenv USE_SYSTEM_7ZA true
 setenv USE_SYSTEM_APP_BUILDER true
 setenv ELECTRON_SKIP_BINARY_DOWNLOAD 1
-setenv ELECTRON_OVERRIDE_DIST_PATH /home/miki/.cache/electron
+setenv ELECTRON_OVERRIDE_DIST_PATH /usr/local/share/electron30
+setenv ESBUILD_BINARY_PATH /home/stuff/signal-desktop/ports/net-im/signal-desktop/work/Signal-Desktop-7.15.0/node_modules/@esbuild/freebsd-x64/bin/esbuild
+
+#setenv USE_SYSTEM_7ZA true
+# setenv ELECTRON_OVERRIDE_DIST_PATH /home/miki/.cache/electron
 
 setenv PATH /home/miki/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/local/sbin:/usr/home/miki/signal-desktop.git/ports/net-im/signal-desktop/work/Signal-Desktop-5.32.0/node_modules/.bin
