a5b86c1ceed8e5851d1beb532912e6a99b587f66
Mikael Urankar <mikael@FreeBSD.org>
2024-12-05 10:15:21 +0100

    Add script to help sync the repo with the ports tree

---
+++ sync_with_ports_tree.sh
@@ -0,0 +1,3 @@
+diff -drup /usr/ports/multimedia/ringrtc ports/multimedia/ringrtc 
+diff -drup /usr/ports/net-im/libsignal-node ports/net-im/libsignal-node
+diff -drup /usr/ports/net-im/signal-desktop ports/net-im/signal-desktop
