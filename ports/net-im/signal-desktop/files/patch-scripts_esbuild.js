4f64ca23927b6444b36d51dd4464c3097b6b3d0e
Mikael Urankar <mikael@FreeBSD.org>
2025-03-14 15:56:53 +0100

    net-im/signal-desktop: doesn't work

---
+++ ports/net-im/signal-desktop/files/patch-scripts_esbuild.js
@@ -0,0 +1,10 @@
+--- scripts/esbuild.js.orig	2025-03-14 15:18:41.432316000 +0100
++++ scripts/esbuild.js	2025-03-14 15:18:48.083918000 +0100
+@@ -37,7 +37,6 @@ const bundleDefaults = {
+     '@signalapp/better-sqlite3',
+     '@indutny/mac-screen-share',
+     'electron',
+-    'fs-xattr',
+     'fsevents',
+     'mac-screen-capture-permissions',
+     'sass',
